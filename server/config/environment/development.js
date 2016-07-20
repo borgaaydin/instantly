c37191c433419dcfd15eb562232e8f4a817d83d0
borga <borgaaydin@gmail.com>
2016-07-20 08:44:06 +0300

    mongo uri

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/paizatter-dev'
+    uri: 'mongodb://heroku_9k0pxpcj:2mv16lo69afqfim30m1gjaj27i@ds023315.mlab.com:23315/heroku_9k0pxpcj'
   },
 
   seedDB: true
